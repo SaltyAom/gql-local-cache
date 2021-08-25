791b1dbe33e1480f3ff0d70bd522a2d35499b779
SaltyAom <aomkirby123@gmail.com>
2021-08-25 21:41:42 +0700

    :blue_book: doc: spacing

--- examples/html/src/main.ts
+++ examples/html/src/main.ts
@@ -4,18 +4,18 @@ import localCache from '@saltyaom/gql-local-cache'
 client.config('https://api.opener.studio/graphql', undefined, [localCache()])
 
 gql(
-	`query GetHentaiById($id: Int!) {
-      getHentaiById(id: $id) {
-        success
-        data {
-          title {
-            display
-			japanese
-          }
+  `query GetHentaiById($id: Int!) {
+    getHentaiById(id: $id) {
+      success
+      data {
+        title {
+          display
+          japanese
         }
       }
     }
-  `,
+  }
+`,
 	{
 		variables: {
 			id: 177013
